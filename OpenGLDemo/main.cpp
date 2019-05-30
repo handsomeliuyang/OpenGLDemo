d7d7ca242d7ec1d9d4e9f44b01784f73385a369e
liuyang <liuyang@58.com>
2019-05-30 20:19:17 +0800

    Initial Commit

---
+++ OpenGLDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLDemo
+//
+//  Created by ly on 2019/5/30.
+//  Copyright © 2019 ly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
